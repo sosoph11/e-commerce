3fdafe3d970e6558f044b5d8422728c1529e595e
ValVoidwalker <valegomez3899@gmail.com>
2024-08-25 18:41:03 -0300

    otro intento
    aaaa

--- js/products.js
+++ js/products.js
@@ -10,18 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
       productsContainer.innerHTML = "";
       categoryTitle.textContent = data.catName;
 
-      data.products.forEach(product => {
-        let productCard = document.createElement("div");
-        productCard.className = "product-card col-md-4";
-        productCard.innerHTML = `
-          <img src="${product.image}" alt="${product.name}">
-          <h2>${product.name}</h2>
-          <p>${product.description}</p>
-          <div class="price">${product.currency} ${product.cost}</div>
-          <div class="sold">${product.soldCount} Vendidos</div>
+      if (data.products) {
+        data.products.forEach(product => {
+          let productCard = document.createElement("div");
+          productCard.className = "product-card col-md-4";
+          productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+            <h2>${product.name}</h2>
+            <p>${product.description}</p>
+            <div class="price">${product.currency} ${product.cost}</div>
+            <div class="sold">${product.soldCount} Vendidos</div>
+          `;
+          productsContainer.appendChild(productCard);
+        });
+      } else {
+        productsContainer.innerHTML = `
+          <div class="alert alert-danger text-center" role="alert">
+            <h4 class="alert-heading">Funcionalidad en desarrollo</h4>
+          </div>
         `;
-        productsContainer.appendChild(productCard);
-      });
+      }
     })
     .catch(error => {
       console.error("Error al cargar los productos:", error);
